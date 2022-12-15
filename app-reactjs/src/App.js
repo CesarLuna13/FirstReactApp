2f4353fbed9a34d3cbf06f6ea825770b93538f15
Ce Luna <cesar_luga@hotmail.com>
2022-12-14 23:36:49 -0600

    Agregando más routing

--- app-reactjs/src/App.js
+++ app-reactjs/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 //import Button from "./components/Button/Button";
 import NavBar from "./components/Nav/Navbar";
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
+import Home from './components/Pages/Home/Home';
 import Supermercado from './components/Pages/Supermercado/Supermercado';
 
 
@@ -13,21 +14,18 @@ import Supermercado from './components/Pages/Supermercado/Supermercado';
 function App() {
   return (
     
-
-    <BrowserRouter>
-      <NavBar/>
-      <Routes>
-        
-        <Route exact path="/supermercado" element={<Supermercado/>} />
-
-      </Routes>
-
-
-      <ItemListContainer greeting="Hola hjfkhyud">
-      </ItemListContainer>
+    <div className='divGeneral'>
       
-    </BrowserRouter>
+        {/* <NavBar/> */}
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/home' element={<Home/>}/>
+          <Route exact path="/supermercado" element={<Supermercado/>} />
+        </Routes>
 
+      {/*<ItemListContainer greeting="Hola hjfkhyud"></ItemListContainer>*/}
+
+    </div>
   );
 }
 
